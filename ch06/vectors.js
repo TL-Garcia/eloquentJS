077c87b81bbf184441465c286bb756e8095dccde
TL-Garcia <tomas.bl.garcia@gmail.com>
2021-03-10 22:42:09 +0100

    complete vector ex from ch06

---
+++ ch06/vectors.js
@@ -0,0 +1,30 @@
+class Vec {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+
+    plus(vector) {
+        const x = this.x + vector.x
+        const y = this.y + vector.y
+
+        return {
+            x,
+            y
+        }
+    }
+
+    minus(vector) {
+        const x = this.x - vector.x
+        const y = this.y - vector.y
+
+        return {
+            x,
+            y
+        }
+    }
+
+    get length() {
+        return Math.sqrt(this.x**2 + this.y**2)
+    }
+}
